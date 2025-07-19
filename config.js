8ba8379c12e786e635cf825ff78ff9f188e3dadf
drakoif <drakomcx3@outlook.com>
2025-07-19 12:39:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTM5NjA4MzY1MTMzMDE3OTA4Mw.GpqH39.IPw8_4I_kCWlErlyVLhFHYBiLizGVv0CNmfQSI",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["708845942266265611", ""], 
+  mongodbUri : "mongodb+srv://drako:!TLAEFT2008@reomcmusic.wmghnje.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "bbc445be18f24858b6245d620000ead4",
+  spotifyClientSecret : "2a6231669b364fdb8d8c1e3c642f93cc",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "To ReoMc", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/43DFNfmwHg",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
